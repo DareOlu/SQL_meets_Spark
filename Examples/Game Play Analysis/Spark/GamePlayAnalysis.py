40172629451fd8dcda223567b4f437196dbef0b0
DareOlu <do@gmail.com>
2022-03-13 20:58:21 +0000

    Updated Example

--- Examples/Game Play Analysis/Spark/GamePlayAnalysis.py
+++ Examples/Game Play Analysis/Spark/GamePlayAnalysis.py
@@ -1,7 +1,8 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Window
 from datetime import datetime
 from pyspark.sql.types import StructType, StructField, IntegerType, DateType
-from pyspark.sql.functions import first, col
+from pyspark.sql.functions import first, col, min, sum, when, datediff, count, round
+
 
 ## Start Spark Session
 spark = SparkSession.builder \
@@ -31,4 +32,19 @@ activity_df = spark.createDataFrame(data = activity, schema = activity_schema )
 # activity_df.printSchema()
 
 # Challenge 1
-activity_df1 = activity_df.groupBy('player_id').agg(first(col('event_date'))).orderBy('event_date').show()
+activity_df1 = activity_df.groupBy('player_id')\
+  .agg(min('event_date').alias('first_login'))\
+  .orderBy('player_id')
+
+
+# Challenge 2
+activity_df2 = activity_df.withColumn('first_login', min('event_date').over(Window.partitionBy('player_id')))\
+  .filter(col('event_date') == col('first_login'))\
+  .select('player_id', 'device_id')
+
+
+# Challenge 3
+windowspec = (Window.partitionBy('player_id').orderBy('event_date').rangeBetween(Window.unboundedPreceding, 0))
+activity_df3 = activity_df.withColumn('games_played_so_far', sum('games_played').over(windowspec))\
+  .select('player_id', 'device_id', 'games_played_so_far')
+
